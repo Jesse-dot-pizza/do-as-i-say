eeddc62035a5041006abddaa802792e409436306
Jesse-dot-pizza <67610256+Jesse-dot-pizza@users.noreply.github.com>
2022-11-17 12:27:06 -0800

    struggling with keystrokes

--- ui.py
+++ ui.py
@@ -6,43 +6,36 @@ import speech_recognition as sr
 import pyaudio
 
 window_dimensions = (500, 500)
-
-class SimpleUI:
-    def __init__(self) -> None:
-        pygame.init()
-        self.success_sound = pygame.mixer.Sound("sfx\clack.wav")
-        self.fail_sound = pygame.mixer.Sound(r"sfx\boing.wav")
-
-
-    def show_prompts(self, expected_values):
-        print(expected_values)
-
-    def play_feedback(self, event_result: bool):
-        if event_result:
-            self.success_sound.play()
-        else:
-            self.fail_sound.play()
-        
+     
 class PygameWindow:
     def __init__(self) -> None:
         pygame.init()
         self.animation_in_progress = threading.RLock()
         self.seconds_between_frames = 0.2
+        self.keyboard_commands = []
         pygame.fastevent.init()
         threading.Thread(target=self.animate).start()
 
-    def get_keyboard_event(self):
+    def get_keyboard_events(self):
+
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    return events.EventFactory.new_keyboard_event(events.Up()) 
+                    self.keyboard_commands.append(events.Up()) 
                 if event.key == pygame.K_DOWN:
-                    return events.EventFactory.new_keyboard_event(events.Down()) 
+                    self.keyboard_commands.append(events.Down()) 
                 if event.key == pygame.K_LEFT:
-                    return events.EventFactory.new_keyboard_event(events.Left()) 
+                    self.keyboard_commands.append(events.Left())
                 if event.key == pygame.K_RIGHT:
-                    return events.EventFactory.new_keyboard_event(events.Right())
-                    print("Key A has been released")
+                    self.keyboard_commands.append(events.Right())
+
+
+    def listen_for_keystrokes(self, duration):
+        self.keyboard_commands = []
+        threading.Thread(target=self.get_keyboard_events).start()
+        
+
+
 
     def animate(self):
         window = pygame.display.set_mode(window_dimensions)
@@ -60,7 +53,7 @@ class PygameWindow:
                 pygame.display.flip()
 
 class SpeechRecognizer(sr.Recognizer):
-    # note we going to have to not use the default google api key for production
+    # TODO: we going to have to not use the default google api key for production
     def __init__(self) -> None:
         super().__init__()
         self.mic = sr.Microphone()
@@ -74,77 +67,24 @@ class SpeechRecognizer(sr.Recognizer):
         return audio_data
 
     def process_audio_data(self, audio_data: sr.AudioData):
-        result = self.recognize_google(audio_data)
+        words_detected = self.recognize_google(audio_data)
+        result = self.create_events(words_detected)
         return result
 
-    def record_audio_to_wav(self):
+    def create_events(self, words_detected: str):
+        word_list = words_detected.split()
+        output_commands = []
+        for word in word_list:
+            if word == "up":
+                output_commands.append(events.Up())
+            elif word == "down":
+                output_commands.append(events.Down())
+            elif word == "left":
+                output_commands.append(events.Left())
+            elif word == "right":
+                output_commands.append(events.Right())
+        return output_commands
 
-        with self.mic as source:
-            print("Say something!")
-            audio = self.record(source, duration=10)
-
-        # write audio to a WAV file
-        with open("sfx\microphone-results.wav", "wb") as f:
-            f.write(audio.get_wav_data())
-
-
-    def recognize_speech_from_mic(recognizer, microphone):
-        """Transcribe speech from recorded from `microphone`.
-
-        Returns a dictionary with three keys:
-        "success": a boolean indicating whether or not the API request was
-                successful
-        "error":   `None` if no error occured, otherwise a string containing
-                an error message if the API could not be reached or
-                speech was unrecognizable
-        "transcription": `None` if speech could not be transcribed,
-                otherwise a string containing the transcribed text
-        """
-        # check that recognizer and microphone arguments are appropriate type
-        if not isinstance(recognizer, sr.Recognizer):
-            raise TypeError("`recognizer` must be `Recognizer` instance")
-
-        if not isinstance(microphone, sr.Microphone):
-            raise TypeError("`microphone` must be `Microphone` instance")
-
-        # adjust the recognizer sensitivity to ambient noise and record audio
-        # from the microphone
-        with microphone as source:
-            recognizer.adjust_for_ambient_noise(source)
-            audio = recognizer.listen(source)
-
-        # set up the response object
-        response = {
-            "success": True,
-            "error": None,
-            "transcription": None
-        }
-
-        # try recognizing the speech in the recording
-        # if a RequestError or UnknownValueError exception is caught,
-        #     update the response object accordingly
-        try:
-            response["transcription"] = recognizer.recognize_google(audio)
-        except sr.RequestError:
-            # API was unreachable or unresponsive
-            response["success"] = False
-            response["error"] = "API unavailable"
-        except sr.UnknownValueError:
-            # speech was unintelligible
-            response["error"] = "Unable to recognize speech"
-
-        return response
-
-    def parse_response(self, response):
-        words_detected = response["transcription"].split()
-        if "up" in words_detected:
-            return events.Up()
-        elif "down" in words_detected:
-            return events.Down()
-        elif "left" in words_detected:
-            return events.Left()
-        elif "right" in words_detected:
-            return events.Right()
 
 pygame_window = PygameWindow()
 speech_rec = SpeechRecognizer()
@@ -157,6 +97,3 @@ def get_responses():
 
     return voice_response
 
-
-result = get_responses()
-print(result)
